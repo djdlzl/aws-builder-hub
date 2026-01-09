7d08524844949b48977dcce58cf643c19fe82501
Harvey <harvey@spoonlabs.com>
2026-01-09 18:25:36 +0900

    initial

--- builder-hub-backend/src/main/kotlin/co/spoonradio/awsbuilderhub/controller/protocol/user/UserProtocol.kt
+++
@@ -1,8 +0,0 @@
-package co.spoonradio.awsbuilderhub.controller.protocol.user
-
-import jakarta.validation.constraints.NotBlank
-
-data class UpdateRoleRequest(
-    @field:NotBlank(message = "Role is required")
-    val role: String
-)
